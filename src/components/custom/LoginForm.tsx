0a9ad00a10b922f356eb5d74b94c03a15264d4cf
devtestdev317-ui <devtestdev317@gmail.com>
2025-09-20 05:46:46 +0000

    fixsing media query

--- src/components/custom/LoginForm.tsx
+++ src/components/custom/LoginForm.tsx
@@ -29,13 +29,13 @@ export function LoginForm() {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center w-full h-screen  overflow-hidden bg-linear-to-b from-[#c0cce8] from-65% to-white  relative">
+        <div className="flex flex-col items-center justify-center w-full h-screen  overflow-hidden bg-linear-to-b from-[#c0cce8] from-65% to-white  relative px-3.5 sm:px-0">
             {
                 !isMobile ? <> <img src="assets/images/img1.png" className="absolute w-[260px] left-[2%] bottom-[2%]" />
                     <img src="assets/images/img2.png" className="absolute w-[350px] right-[2%] top-[2%]" /></> : null
             }
 
-            <Card className="w-[330px] rounded-sm relative z-10 bg-[#dfe6f6] border-white">
+            <Card className="max-w-[330px] rounded-sm relative z-10 bg-[#dfe6f6] border-white sm:max-w-[320px] w-full">
                 <CardHeader className="flex flex-col items-center gap-3">
                     <img src="assets/images/logo.png" className="w-[130px]" />
                 </CardHeader>
@@ -73,14 +73,14 @@ export function LoginForm() {
                                 )}
                             />
                             {/* isShowPassord, hidePassword */}
-                            <div className="text-[12px] text-right text-slate-600">Having trouble in login <Link className="text-blue-500" to="/" >Click here</Link></div>
-                            <div className="mt-2">
+                            <div className="mt-1">
                                 <div className="flex items-center gap-3">
-                                    <Checkbox id="terms" />
+                                    <Checkbox id="terms" className="bg-transparent border-blue-500 w-[25px] h-[22px] data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500" />
                                     <Label htmlFor="terms">Accept terms and conditions</Label>
                                 </div>
                             </div>
                             <Button className="h-[40px] cursor-pointer">Authenticate <ArrowRight /></Button>
+                            <div className="text-[12px] text-center text-slate-600">Having trouble in login <Link className="text-blue-500" to="/" >Click here</Link></div>
                         </form>
                     </Form>
 
