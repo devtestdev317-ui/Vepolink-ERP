34e032b851603f1509a7b7897764a4d0d8264271
devtestdev317-ui <devtestdev317@gmail.com>
2025-09-20 07:52:30 +0000

    adding Dashboard Page

---
+++ src/components/custom/app-sidebar.tsx
@@ -0,0 +1,177 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/custom/nav-main"
+import { NavSecondary } from "@/components/custom/nav-secondary"
+import { NavUser } from "@/components/custom/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "admin",
+    email: "admin@gmail.com",
+    avatar: "/assets/images/profile.png",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Pre Sales / Lead",
+      url: "#",
+      icon: IconListDetails,
+    },
+    {
+      title: "Sales",
+      url: "#",
+      icon: IconChartBar,
+    },
+    {
+      title: "Accounts",
+      url: "#",
+      icon: IconFolder,
+    },
+    {
+      title: "Service",
+      url: "#",
+      icon: IconUsers,
+    },
+    {
+      title: "IT support",
+      url: "#",
+      icon: IconDatabase,
+    },
+    {
+      title: "IT Department",
+      url: "#",
+      icon: IconFileWord,
+    },
+    {
+      title: "HR Department",
+      url: "#",
+      icon: IconReport,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: IconCamera,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: IconFileDescription,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: IconFileAi,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ]
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-bold">ERP Vepolink</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
--- src/layout/Dashboard-layout.tsx
+++ src/layout/Dashboard-layout.tsx
@@ -1,7 +1,32 @@
 import { Outlet } from "react-router-dom";
 
+import { AppSidebar } from "@/components/custom/app-sidebar";
+import { SiteHeader } from "@/components/custom/site-header";
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
 export default function DashboardLayout() {
     return (
-        <Outlet />
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" collapsible="icon" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 p-4 md:gap-6 md:py-6">
+                            <Outlet />
+                        </div>
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
